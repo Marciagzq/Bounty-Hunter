471e0f3671c6b5c1a4631e11f3837651f5a53a32
Marcia Zegarra <marcia.gzq@gmail.com>
2019-11-02 19:01:27 -0400

    character movement functionality added

--- sempiternal/src/Components/Player/move.js
+++ sempiternal/src/Components/Player/move.js
@@ -1,3 +1,4 @@
+import React from "react";
 import store from "../../Config/store";
 import { spriteSize, mapWidth, mapHeight } from "../../Config/constants";
 
@@ -18,6 +19,26 @@ export default function handleMovement(player) {
         }
     }
 
+    //function to display different character
+    function getSpriteLocation(direction, walkIndex) {
+        switch(direction) {
+            case "West":
+                return `${spriteSize*walkIndex}px ${spriteSize*2}px`
+            case "East":
+                return `${spriteSize*walkIndex}px ${spriteSize*1}px`
+            case "North":
+                return `${spriteSize*walkIndex}px ${spriteSize*3}px`
+            case "South":
+                return `${spriteSize*walkIndex}px ${spriteSize*0}px` 
+        }
+    }
+
+    function getWalkIndex() {
+        const walkIndex = store.getState().player.walkIndex 
+            return walkIndex >= 7 ? 0 : walkIndex + 1
+    }
+      
+
     //checks boundaries when the player moves
     function observeBoundaries(oldPos, newPos) {
         return (newPos[0] >= 0 && newPos[0] <= mapWidth - spriteSize) &&
@@ -34,11 +55,15 @@ export default function handleMovement(player) {
     }
 
     //dispatches the getNewPosition function to the store named as move_Player
-    function dispatchMove(newPos) {
+    function dispatchMove(direction, newPos) {
+        const walkIndex = getWalkIndex()
         store.dispatch({
             type: "move_Player",
             payload: {
                 position: newPos,
+                direction: direction,
+                walkIndex,
+                spriteLocation: getSpriteLocation(direction, walkIndex),
             }
         })
     }
@@ -48,7 +73,7 @@ export default function handleMovement(player) {
         const newPos = getNewPosition(oldPos, direction)
 
         if(observeBoundaries(oldPos, newPos) && observeObstacles(oldPos, newPos)) {
-            dispatchMove(newPos)
+            dispatchMove(direction, newPos)
         }
     }
 
