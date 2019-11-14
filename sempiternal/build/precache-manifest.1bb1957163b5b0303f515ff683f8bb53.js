2b8d054a3beead51729911e9c1a7082df4524603
Marcia Zegarra <marcia.gzq@gmail.com>
2019-11-13 19:31:54 -0500

    heroku tryout 3

---
+++ sempiternal/build/precache-manifest.1bb1957163b5b0303f515ff683f8bb53.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7940bf8cec6bcf531b6ad8be91c8c1ac",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0f1374f2cd46e69f6047",
+    "url": "/static/css/main.d9a3bd64.chunk.css"
+  },
+  {
+    "revision": "37ff97191a9c923a3c4e",
+    "url": "/static/js/2.56054e69.chunk.js"
+  },
+  {
+    "revision": "0f1374f2cd46e69f6047",
+    "url": "/static/js/main.16a55ebe.chunk.js"
+  },
+  {
+    "revision": "0befdcab64d6f29ba7dd",
+    "url": "/static/js/runtime-main.f80be3b4.js"
+  },
+  {
+    "revision": "ca4e029c525e8dcd23cba7e00e751ad9",
+    "url": "/static/media/mageAttackLeft.ca4e029c.gif"
+  },
+  {
+    "revision": "88cc1dfc9528a41ee12026a7843b7926",
+    "url": "/static/media/mageAttackRight.88cc1dfc.gif"
+  },
+  {
+    "revision": "50aaf403b10a3c742f8d4a744bdedee2",
+    "url": "/static/media/mageLeft.50aaf403.gif"
+  },
+  {
+    "revision": "0a90baa4dcba74d9d4bfcf90f9cb7d4c",
+    "url": "/static/media/mageRight.0a90baa4.gif"
+  },
+  {
+    "revision": "9f19440f74f1c8126fdcf003e3d024da",
+    "url": "/static/media/player_walk.9f19440f.png"
+  }
+]);
