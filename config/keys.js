843ff06e9092472b7a7008716748f1647ccda862
Marcia Zegarra <marcia.gzq@gmail.com>
2019-11-13 21:57:15 -0500

    mlab working.

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,5 @@
 module.exports = {
-    mongoURI: "mongodb://game:game123@ds241895.mlab.com:41895/heroku_4zr8bg5w",
+    mongoURI: "mongodb://game:game123@ds241895.mlab.com:41895/heroku_4zr8bg5w" || "mongodb://localhost:27017/gamedb",
     // maybe to change
     cypher: "secret"
 };
